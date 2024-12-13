821eb91c118d0cd81e5dc0be1d0269313c1aa9fd
xiatingyu <xiatingyu1012@163.com>
2024-12-13 19:26:25 +0800

    upload

---
+++ diverse/qwen2_openhermes.sh
@@ -0,0 +1,16 @@
+source activate dpo
+pip install wandb
+pip install sentence_transformers
+
+export TOKENIZERS_PARALLELISM=false
+
+DISTRIBUTED_ARGS="--nproc_per_node ${KUBERNETES_CONTAINER_RESOURCE_GPU} \
+                    --nnodes ${WORLD_SIZE} \
+                    --node_rank ${RANK} \
+                    --master_addr ${MASTER_ADDR} \
+                    --master_port 6655"
+
+
+torchrun $DISTRIBUTED_ARGS train.py \
+        --config_file ./configs/config_openhermes_qwen2.yml \
+        --wandb_key cac878fde2ea0334527a15036103638df587d233 
